a59cf4944c369ad3665d7342532097aa5300d04a
ferhatkaymaz2023 <ferhatkaymaz2023@gmail.com>
2021-10-31 14:42:07 +0300

    ShowAlert fonksiyonu yazıldı

--- js/app.js
+++ js/app.js
@@ -6,5 +6,43 @@ const filter = document.getElementById("filter");
 const todoList = document.querySelector(".list-group");
 const clearButton = document.getElementById('clear-todos');
 
-(function(){
-});
+
+addEventListeners();
+
+function addEventListeners()
+{
+    form.addEventListener('submit',addTodo);
+}
+
+
+
+
+function addTodo(event)
+{   
+    const newTodo = todoInput.value.trim();
+
+    if(newTodo === "")
+    {
+        showAlert('danger','Boş değer girdiniz.');
+    }
+    else 
+    {
+        showAlert('success','Başarıyla eklendi !');
+    }
+
+    event.preventDefault();
+}
+
+
+const showAlert = (type,message) => 
+{
+    const div = document.createElement('div');
+    div.className = `alert alert-${type}`;
+    div.textContent = message;
+
+    firstCardBody.appendChild(div);
+
+    setTimeout(() => {
+        div.remove();
+    },1000);
+}
