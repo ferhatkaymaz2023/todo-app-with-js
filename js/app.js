174954f13a256af5c9367dfbbdfa2c00b5a66c71
ferhatkaymaz2023 <ferhatkaymaz2023@gmail.com>
2021-10-31 14:45:58 +0300

    addTodoToUI fonksiyonu yazılmıştır

--- js/app.js
+++ js/app.js
@@ -27,10 +27,27 @@ function addTodo(event)
     }
     else 
     {
+        addTodoToUI(newTodo);
         showAlert('success','Başarıyla eklendi !');
     }
 
     event.preventDefault();
+}
+
+const addTodoToUI = (newTodo) => 
+{
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+
+    li.className = `list-group-item d-flex justify-content-between`;
+    li.appendChild(document.createTextNode(newTodo));
+    a.id = '#';
+    a.className = 'delete-item';
+    a.innerHTML = `<i class = "fa fa-remove"></i>`;
+    li.appendChild(document.createTextNode(a));
+
+    todoList.appendChild(li);
+
 }
 
 
