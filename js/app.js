fd1128bcfdc8132caec41cf1c28c9306f972fc75
ferhatkaymaz2023 <ferhatkaymaz2023@gmail.com>
2021-10-31 14:35:28 +0300

    Proje başlangıç

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const form = document.getElementById("todo-form");
+const todoInput = document.getElementById("todo");
+const filter = document.getElementById("filter");
+const todoList = document.querySelector(".list-group");
+const clearButton = document.getElementById('clear-todos');
+
+(function(){
+});
